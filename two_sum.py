aefdf11541df8c92a40023be4b6ec809bc7a7aaa
Nozhan <nozhan@zarinplus.com>
2025-05-20 19:34:41 +0330

    refactor two sum

--- two_sum.py
+++ two_sum.py
@@ -1,18 +1,37 @@
+"""Two Sum: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+link: https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/546/
+"""
+
 class Solution:
-    @staticmethod
-    def find_target(nums, target):
+
+    def hash_table(self, nums, target):
+        """Time complexity: O(n) Space complexity: O(n)"""
 
         num_indices = {}
         for index, num in enumerate(nums):
-            print(num_indices)
 
             diff = target - num
             print(diff)
-            if diff in num_indices:
+            if diff in nums:
                 return [num_indices[diff], index]
 
             num_indices[num] = index
 
         return []
 
-print(Solution.find_target([7,1, 6, 2, 4, 3], 5))
+    def bruteforce(self, nums, target):
+        """Time complexity: O(n2) Space complexity: O(1)"""
+
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+
+print(Solution().hash_table([7,1, 6, 2, 4, 3], 5))
