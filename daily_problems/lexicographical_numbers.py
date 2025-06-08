bba3c8d8d3eb7ada10df3cb915ab3c37005a73bf
Nozhan <nozhan@zarinplus.com>
2025-06-08 19:15:52 +0330

    add lexical order

---
+++ daily_problems/lexicographical_numbers.py
@@ -0,0 +1,26 @@
+"""Lexicographical Numbers: Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+
+You must write an algorithm that runs in O(n) time and uses O(1) extra space.
+
+link: https://leetcode.com/problems/lexicographical-numbers/description/
+"""
+
+class Solution:
+
+    def lexical_order(self, n: int):
+        """Time complexity: O(n) Space complexity: O(1)"""
+
+        result = []
+        curr = 1
+        for _ in range(n):
+            result.append(curr)
+            if curr * 10 <= n:
+                curr *= 10
+            else:
+                while curr % 10 == 9 or curr + 1 > n:
+                    curr //= 10
+                curr += 1
+        return result
+
+# Example Usage:
+# print(Solution().lexical_order(13))
